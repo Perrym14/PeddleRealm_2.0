be1a38dd32ca44d008b0bf0d28936d70b32c4b11
Perrym14 <malikap14@gmail.com>
2019-10-18 15:14:20 -0400

    Added ShoppingCart model and fleshed out respositories.

--- Interfaces/IItemRepository.cs
+++ Interfaces/IItemRepository.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PeddleRealmCore.Models;
 
 namespace PeddleRealmCore.Interfaces
 {
     public interface IItemRepository
     {
+        Item GetItem(int itemId);
+        void Add(Item item);
+        IEnumerable<Item> GetItems();
     }
 }
--- Interfaces/IShoppingCartRepository.cs
+++ Interfaces/IShoppingCartRepository.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using PeddleRealmCore.Models;
 
 namespace PeddleRealmCore.Interfaces
 {
     public interface IShoppingCartRepository
     {
+        void AddToCart();
+        void EmptyCart();
+        List<Cart> GetCartItems();
+        int GetCount();
+        decimal GetTotal();
+        /*Write signature for:
+         GetCart(HttpContext)
+         GetCart(Controller helper method)
+         RemoveFromCart(returns itemCount)
+         GetCartItems
+         CreateOrder
+         GetCartId
+         MigrateCart
+         */
+
     }
 }
